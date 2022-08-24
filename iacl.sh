e1fdaaaf602796496577ea94e1326ea75eb03a26
Bianca Albuquerque <42790720+BiancaAdS@users.noreply.github.com>
2022-08-24 18:48:05 -0300

    Adicionado script.

---
+++ iacl.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo "Criando os Diretórios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando os Grupos de Usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_SEC
+groupadd GRP_VEN
+
+echo "Criando os Usuários e adicionando aos grupos..."
+
+useradd carlos -c "Carlos" -s /bin/bash -p $(openssl passwd Senha123) -m -G GRP_ADM
+passwd carlos -e
+useradd maria -c "Maria" -s /bin/bash -p $(openssl passwd Senha123) -m -G GRP_ADM
+passwd maria -e
+userdd joao -c "Joao" -s /bin/bash -p $(openssl passwd Senha123) -m -G GRP_ADM
+passwd joao -e
+
+useradd debora -c "Debora" -s /bin/bash -p $(openssl passwd Senha123) -m -G GRP_VEN
+passwd debora -e
+useradd sebastiana -c "Sebastiana" -s /bin/bash -p $(openssl passwd Senha123) -m -G GRP_VEN
+passwd sebastiana -e
+useradd roberto -c "Roberto" -s /bin/bash -p $(openssl passwd Senha123) -m -G GRP_VEN
+passwd roberto -e
+
+adduser josefina -c "Josefina" -s /bin/bash -p $(openssl passwd Senha123) -m -G GRP_SEC
+passwd josefina -e
+adduser amanda -c "Amanda" -s /bin/bash -p $(openssl passwd Senha123) -m -G GRP_SEC
+passwd amanda -e
+adduser rogerio -c "Rogerio" -s /bin/bash -p $(openssl passwd Senha123) -m -G GRP_SEC
+passwd rogerio -e
+
+echo "Especificando permissões dos diretórios..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Finalizado criação."
